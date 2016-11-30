bf5bc2ff3e2aff27e8ee1bcdac7448996978cbee
Ryan Burns <ryan@ryanburnsdesigns.com>
2016-11-30 12:36:22 -0800

    create shared link

---
+++ Folders/create-shared-link.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Create Shared Link - https://docs.box.com/reference#create-a-shared-link-for-a-file
+Used to create a shared link for this particular file.
+*/
+
+function createSharedLink($folderid, $access_token, $access){
+  $a = ["access" => $access];
+  $data = array("shared_link" => $a);
+  $header = array();
+  $header[] = 'Authorization: Bearer'.' '.$access_token;
+  $options = array(
+  CURLOPT_URL => 'https://api.box.com/2.0/folders/'.$folderid,
+  CURLOPT_HTTPHEADER => $header,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "PUT",
+  CURLOPT_POSTFIELDS => json_encode($data),
+  );
+  $ch = curl_init();
+  curl_setopt_array($ch, $options);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return json_decode($result);
+}
+
+//createSharedLink($folderid, $access_token, "collaborators");
